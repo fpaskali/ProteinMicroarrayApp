4426ab2613d0798b5fcb96fce611a303e3929e56
fpaskali <paskali2005@gmail.com>
2022-04-19 15:08:24 +0200

    fixed bugs

--- R/proteinarray_app.R
+++ R/proteinarray_app.R
@@ -611,8 +611,9 @@ server <- function(input, output, session) {
     
     for (row in 1:nrow(labels)){
       cell <- list()
-      cell[1] <- substring(labels[row,1],1,1)
-      cell[2] <- substring(labels[row,1],2)
+      splitted_str <- strsplit(labels[row,1],split="(?<=[a-zA-Z])\\s*(?=[0-9])",perl=TRUE)
+      cell[1] <- splitted_str[[1]][1]
+      cell[2] <- splitted_str[[1]][2]
       cell <- c(match(cell[1], LETTERS), as.numeric(cell[2]))
       idx <- match(labels[row,2],names)
       
